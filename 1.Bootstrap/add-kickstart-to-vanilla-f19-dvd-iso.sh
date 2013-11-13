8ab5dbe28f1ccf0a40c76e647f3b60d55a4c1e2a
Craig J Perry <craigp84@gmail.com>
2013-11-13 18:37:09 +0000

    Bugfix iso creation

--- 1.Bootstrap/add-kickstart-to-vanilla-f19-dvd-iso.sh
+++ 1.Bootstrap/add-kickstart-to-vanilla-f19-dvd-iso.sh
@@ -13,8 +13,10 @@ MKISOFS="${DEFANG}mkisofs"
 MKDIR="${DEFANG}mkdir"
 RMDIR="${DEFANG}rm -rf"
 CHMOD="${DEFANG}chmod"
+ISOINFO="${DEFANG}isoinfo"
 
 _TEMP_DIR="/var/tmp/$(basename ${0} ".sh").$$.workdir"
+_ISO_DIR="${_TEMP_DIR}/iso"
 _TEMP_MOUNT="/media/src-iso"
 
 
@@ -56,17 +58,25 @@ function unmount_iso
     ${SUDO} rmdir ${_TEMP_MOUNT}
 }
 
+function write_iso
+{
+    VOLUME_NAME="$(${ISOINFO} -d -i ${1} | grep 'Volume id' | cut -f3- -d' ')"
+    OUTPUT_NAME="$(basename ${1} '.iso').patched.iso"
+    ${MKISOFS} -R -J -T -v -no-emul-boot -boot-load-size4 -boot-info-table -V "${VOLUME_NAME}" -b isolinux/isolinux.bin -c isolinux/boot.cat -o "${_TEMP_DIR}/${OUTPUT_NAME}" ${_ISO_DIR}
+}
+
 function main
 {
     cd $(dirname ${0})
     mount_iso ${1}
-    ${MKDIR} $_TEMP_DIR
-    ${SUDO} cp -a ${_TEMP_MOUNT}/. ${_TEMP_DIR}
-    ${SUDO} chown -R ${UID} ${_TEMP_DIR}
-    ${CHMOD} -R u+rw ${_TEMP_DIR}
-    ${CP} d1.ks ${_TEMP_DIR}/
-    ${CP} isolinux.cfg ${_TEMP_DIR}/isolinux/
-    ${RMDIR} ${_TEMP_DIR}
+    ${MKDIR} -p ${_ISO_DIR}
+    ${SUDO} cp -a ${_TEMP_MOUNT}/. ${_ISO_DIR}
+    ${SUDO} chown -R ${UID} ${_ISO_DIR}
+    ${CHMOD} -R u+rw ${_ISO_DIR}
+    ${CP} d1.ks ${_ISO_DIR}/
+    ${CP} isolinux.cfg ${_ISO_DIR}/isolinux/
+    write_iso ${1}
+    ${RMDIR} ${_ISO_DIR}
     unmount_iso
 }
 
@@ -82,13 +92,3 @@ case "${1}" in
     ;;
 esac
 
-# From notes:
-#    $ wget http://download.fedoraproject.org/pub/fedora/linux/releases/19/Fedora/x86_64/iso/Fedora-19-x86_64-DVD.iso
-#    $ sudo mount -o loop Fedora-19-x86_64-DVD.iso /mnt
-#    $ mkdir f19
-#    $ cp -a /mnt/. f19  # NB: The . is required
-#    $ sudo umount /mnt
-#    $ cp ~/d1.ks f19
-#    $ cp ~/isolinux.cfg f19
-#    $ cd f19
-#    $ mkisofs -R -J -T -v -no-emul-boot -boot-load-size4 -boot-info-table -V "Fedora 19 x86_64" -b isolinux/isolinux.bin -c isolinux/boot.cat -o ~/f19-d1.iso .
