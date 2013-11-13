69466fb0d2648501d848546a3398b59c7e03dfda
Craig J Perry <craigp84@gmail.com>
2013-11-13 14:37:07 +0000

    Added fake/real command switch ability, supports --dry-run option

--- 1.Bootstrap/add-kickstart-to-vanilla-f19-dvd-iso.sh
+++ 1.Bootstrap/add-kickstart-to-vanilla-f19-dvd-iso.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 #
 # Extract ISO file, insert kickstart, patch isolinux.cfg, rebuild iso
+#
 
+
+# Commands
+FAKE_CP="echo cp"
+REAL_CP="/bin/cp"
+CP="${FAKE_CP}"
+
+FAKE_WGET="echo wget"
+REAL_WGET="/usr/bin/wget"
+WGET="${FAKE_WGET}"
+
+FAKE_SUDO="echo sudo"
+REAL_SUDO="/usr/bin/sudo"
+SUDO="${FAKE_SUDO}"
+
+FAKE_MKISOFS="echo mkisofs"
+REAL_MKISOFS="/usr/bin/mkisofs"
+MKISOFS="${FAKE_MKISOFS}"
+
+FAKE_MKDIR="echo mkdir"
+REAL_MKDIR="/usr/bin/mkdir"
+MKDIR="${FAKE_MKDIR}"
 
 # From notes:
 #    $ wget http://download.fedoraproject.org/pub/fedora/linux/releases/19/Fedora/x86_64/iso/Fedora-19-x86_64-DVD.iso
