b0703b00b236d42c51c8eff1a10c882e9b3d5a4a
Craig J Perry <craigp84@gmail.com>
2013-11-16 01:57:52 +0000

    Setup python path for tests

--- 3.Hacking/tests/__init__.py
+++ 3.Hacking/tests/__init__.py
@@ -6,3 +6,13 @@ Testing of Ansible playbooks.
    Craig J Perry <craigp84@gmail.com>
 """
 
+
+import sys
+from os.path import dirname, join, abspath
+
+
+def setUpPackage():
+    "Ensure tests package is importable from within tests"
+    sys.path.insert(0, abspath(join(dirname(__file__), "..")))
+
+
