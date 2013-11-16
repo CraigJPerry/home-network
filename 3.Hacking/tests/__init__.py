135a0707118dfdf10ff2d36730fc00ea2cc9b312
Craig J Perry <craigp84@gmail.com>
2013-11-16 02:02:54 +0000

    Added fileContains / fileDoesntContain methods (with tests)

--- 3.Hacking/tests/__init__.py
+++ 3.Hacking/tests/__init__.py
@@ -8,6 +8,7 @@ Testing of Ansible playbooks.
 
 
 import sys
+import re
 import unittest
 from os.path import dirname, join, abspath, exists, isfile, isdir, islink
 
@@ -34,6 +35,15 @@ class FileSystemAssertsMixin(object):
         "Confirm filepath doesn't exist"
         self.assertTrue(not exists(filepath))
 
+    def assertFileContains(self, filepath, count, regex):
+        "Check if filepath contains count occurances of regex"
+        with open(filepath, "r") as fhandle:
+            matches = sum(len(re.findall(regex, line)) for line in fhandle.xreadlines())
+        self.assertEqual(count, matches)
+
+    def assertFileDoesntContain(self, filepath, regex):
+        return self.assertFileContains(filepath, 0, regex)
+
 
 class TestFileSystemAssertsMixin(unittest.TestCase, FileSystemAssertsMixin):
 
@@ -42,4 +52,11 @@ class TestFileSystemAssertsMixin(unittest.TestCase, FileSystemAssertsMixin):
 
     def test_file_doesnt_exist(self):
         self.assertFileNotExists("foo")
+
+    def test_file_contains(self):
+        self.assertFileContains("/etc/passwd", 4, "root")
+        self.assertFileContains("/etc/passwd", 1, "^root")
+
+    def test_file_doesnt_contain(self):
+        self.assertFileDoesntContain("/etc/passwd", "DonkeyKongRacer")
 
