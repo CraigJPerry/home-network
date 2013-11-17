d2564c1a00659da81c1fd9c39f8d751004d14339
Craig J Perry <craigp84@gmail.com>
2013-11-17 20:34:39 +0000

    Test case to check that doesnt_exist correctly raises for a file that does exist

--- tests/test_framework.py
+++ tests/test_framework.py
@@ -24,6 +24,9 @@ class TestFileSystemAssertsMixin(Pep8TestCase, FileSystemAssertsMixin):
     def test_file_doesnt_exist(self):
         self.assert_file_doesnt_exist("foo")
 
+    def test_doesnt_exist_fails_when_file_does_exist(self):
+        self.assertRaises(AssertionError, self.assert_file_doesnt_exist, __file__)
+
     def test_file_contains(self):
         self.assert_file_contains("/etc/passwd", 4, "root")
         self.assert_file_contains("/etc/passwd", 1, "^root")
