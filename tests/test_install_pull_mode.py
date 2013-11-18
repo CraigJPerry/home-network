5e2d791c1d6eb811fafc21dd3ceb2658b4276f80
Craig J Perry <craigp84@gmail.com>
2013-11-18 00:50:53 +0000

    Added test case for package installation by playbook

--- tests/test_install_pull_mode.py
+++ tests/test_install_pull_mode.py
@@ -8,15 +8,21 @@ Testing of install-pull-mode playbook.
 
 
 from os.path import join, dirname, pardir
-from tests.framework import AnsiblePlayTestCase
+from tests.framework import AnsiblePlayTestCase, PackageAssertsMixin, remove_package
 from getpass import getuser
 
 
-class TestInstallAsNonRootViaSudo(AnsiblePlayTestCase):
+class TestInstallAsNonRootViaSudo(AnsiblePlayTestCase, PackageAssertsMixin):
 
     PLAYBOOK = join(dirname(__file__), pardir, "install-pull-mode.yml")
 
     def test_case_setup_correctly(self):
         self.assert_true(getuser() != "root")
         self.assert_file_exists(self.PLAYBOOK)
+
+    def test_installs_git(self):
+        remove_package("git", force=True)
+        self.assert_package_not_installed("git")
+        self.play()
+        self.assert_package_installed("git")
 
