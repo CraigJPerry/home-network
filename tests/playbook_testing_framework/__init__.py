834ee8aa07ac22c19b77e5ab996aa5d9ac62f69d
Craig J Perry <craigp84@gmail.com>
2013-11-18 23:18:32 +0000

    Cleanup inheritance tree for AnsibleTestCase. Improve comments

--- tests/playbook_testing_framework/__init__.py
+++ tests/playbook_testing_framework/__init__.py
@@ -30,7 +30,7 @@ class AnsiblePlaybookError(Exception):
     pass
 
 
-class AnsiblePlayTestCase(Pep8TestCase, FileSystemAssertsMixin):
+class AnsiblePlayTestCase(Pep8TestCase):
     """TestCase for ansible play testing.
 
     Subclass this rather than unittest.TestCase when writing tests for
@@ -38,30 +38,38 @@ class AnsiblePlayTestCase(Pep8TestCase, FileSystemAssertsMixin):
 
     Redefine cls.PLAYBOOK with the path to the playbook to be tested.
 
-    Write test_xxx() methods as normal. Group related tests in their own
+    You can write test_xxx() methods as you normally would with python's
+    unittest framework. E.g. you can group related tests in their own
     subclasses of AnsiblePlayTestCase.
 
     Call the self.play() method in your tests to invoke the playbook,
     all ansible output on stdout or stderr will be returned from this
-    call.
+    call. Sometimes it's useful to print this when debugging a play!
 
-    Use the PEP8-compliant assert methods in your tests:
+    You can use the PEP8-compliant assert methods in your tests:
 
     * assert_true
     * assert_equal
     * assert_in
     * assert_raises
-    * assert_file_exists
-    * assert_file_doesnt_exist
-    * assert_file_contains
-    * assert_file_doesnt_contain
 
-    A failure in the ``ansible-playbook`` run with raise
+    And freely mixin assert methods in your TestCase's inheritence tree:
+
+    >>> from playbook_testing_framework import AnsiblePlayTestCase
+    >>> from playbook_testing_framework.mixins import FileSystemAssertsMixin
+    >>>
+    >>> class TestCaseExample(AnsiblePlayTestCase, FileSystemAssertsMixin):
+    >>>     def test_something(self):
+    >>>         self.assert_file_exists("/path/to/file")  # Time saved!
+
+    See the ``playbook_testing_framework.mixins`` package for more.
+
+    A failure in the ``ansible-playbook`` run will raise
     AnsiblePlaybookError.
     """
 
-    INVENTORY = join(ROOT_DIR, "hosts-testing")
-    PLAYBOOK = ""
+    PLAYBOOK = ""  # Set this file path in your subclass
+    INVENTORY = join(ROOT_DIR, "hosts-testing")  # Rarely need to override
 
     def play(self):
         "Invoke the playbook associated with this TestCase"
