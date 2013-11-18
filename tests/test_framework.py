97152d65222cc05202b970f164d51e77a53030ba
Craig J Perry <craigp84@gmail.com>
2013-11-18 23:30:21 +0000

    Relocated TestCase framework tests

--- tests/test_framework.py
+++ tests/test_framework.py
@@ -46,46 +46,6 @@ class TestFileSystemAssertsMixinContains(Pep8TestCase, FileSystemAssertsMixin):
         self.assert_raises(AssertionError, self.assert_file_doesnt_contain, "/etc/passwd", "root")
 
 
-class TestAnsiblePlayTestCase(AnsiblePlayTestCase):
-
-    PLAYBOOK = join(FIXTURES_DIR, "TestAnsiblePlayTestCase.yml")
-
-    def test_can_invoke_playbook(self):
-        output = self.play()
-        self.assert_in('ok: [10.78.19.84] => {"msg": "Hello, World!"}', output)
-
-    def test_play_output_is_broken_into_list_of_lines(self):
-        output = self.play()
-        self.assert_true(type(output) is list)
-
-    def test_INVENTORY_exists(self):
-        self.assert_file_exists(self.INVENTORY, "file")
-
-
-class TestNonExistantPlaybook(AnsiblePlayTestCase):
-
-    PLAYBOOK = "DoesntExist.yml"
-
-    def test_play_invocation_flags_failure_to_test_framework(self):
-        self.assert_raises(AnsiblePlaybookError, self.play)
-
-    def test_exception_contains_return_code(self):
-        try:
-            self.play()
-        except AnsiblePlaybookError as ex:
-            self.assert_in("return code [1]", ex.message)
-        else:
-            self.fail("Exception wasn't raised for failed ansible-playbook run")
-
-    def test_exception_contains_reason(self):
-        try:
-            self.play()
-        except AnsiblePlaybookError as ex:
-            self.assert_in("because [ERROR: the playbook: DoesntExist.yml could not be found]", ex.message)
-        else:
-            self.fail("Exception wasn't raised for failed ansible-playbook run")
-
-
 class TestPackageAssertsMixinNotInstalled(Pep8TestCase, PackageAssertsMixin):
 
     def test_package_not_installed(self):
